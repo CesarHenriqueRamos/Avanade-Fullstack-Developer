3649057e2d457d1ab9218042a3299cadbc3de5a0
Cesar Henrique Ramos <cesar252742@gmail.com>
2020-10-27 18:56:05 -0300

    Curso de dotnet 27-10-2020

---
+++ dotnet-inicial/DigitalInovationOne/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace DigitalInovationOne
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int fim = 6;
+            for (int i = 1; i < fim; i++)
+            {
+               Console.WriteLine("numero "+ i); 
+               Console.WriteLine($"numero {i}"); 
+            }
+            
+        }
+    }
+}
